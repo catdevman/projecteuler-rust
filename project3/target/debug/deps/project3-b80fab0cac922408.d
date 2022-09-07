b9b9fafedde43faed11d8a4eaeea9ff6b4e07ac5
Lucas Pearson <lpearson@trek10.com>
2022-09-07 00:12:21 -0400

    add gitignore; remove target directory from project3

--- project3/target/debug/deps/project3-b80fab0cac922408.d
+++
@@ -1,5 +0,0 @@
-/Users/lucaspearson/code/personal/github.com/catdevman/projecteuler-rust/project3/target/debug/deps/project3-b80fab0cac922408: src/main.rs
-
-/Users/lucaspearson/code/personal/github.com/catdevman/projecteuler-rust/project3/target/debug/deps/project3-b80fab0cac922408.d: src/main.rs
-
-src/main.rs:
